4adb62da5d1176fc7bb3f84bc598fc0b8e306905
Miguel-Lopez-97 <miguel.m.lopez.p@gmail.com>
2022-02-16 16:42:53 -0500

    CSS responsive MenuHome component

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,8 +1,11 @@
+import React from "react";
+import { MenuHome } from "./menu-home/MenuHome";
 
 export const Home = () =>  {
     return (
         <div className="home"> 
             escribe codigo aqui Home
+            <MenuHome/>
         </div>
     )
 }
--- client/src/components/home/menu-home/MenuHome.jsx
+++ client/src/components/home/menu-home/MenuHome.jsx
@@ -1,9 +1,15 @@
-
+import React from "react";
+import "./menuhome.css"
 
 export const MenuHome = () =>  {
     return (
-        <div className="menuhome"> 
-            escribe codigo aqui 
+        <div className="menuHome"> 
+        <h4>Categories</h4>
+            <ul>
+                <button className="all">All</button>
+                <button>Products</button>
+                <button>Experiences</button>
+            </ul>
         </div>
     )
 }
