c4f9d3b6e680dd8cab83cc02c0024d1c4cad75e1
Miguel-Lopez-97 <miguel.m.lopez.p@gmail.com>
2022-02-16 17:50:45 -0500

    CSS examples changes in home component

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { MenuHome } from "./menu-home/MenuHome";
 import { Carousel } from "./carousel/Carousel";
+import "./home.css"
 
 export const Home = () =>  {
   
