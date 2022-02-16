aa945eb2890a5ca7a5dff03ea97c6abf2fdf32c1
Miguel-Lopez-97 <miguel.m.lopez.p@gmail.com>
2022-02-16 16:23:07 -0500

    CSS responsive form Navbar and Header components

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./navbar.css"
 import naturalyLogo from "../../assets/img/leaft.png"
+import login from "../../assets/img/login.png";
 
 export const Navbar = () => {
   return (
@@ -11,12 +12,13 @@ export const Navbar = () => {
         <div className="nameCompany"><h1>Be Natural</h1></div>
         <div className="sloganCompany"><h3>The best way to fell good</h3></div>
       </div>
-      <div>
+      <div className="navbar">
         <ul>
           <Link to={`./`}>Home</Link>
           <Link to={`./Products`}>Products</Link>
           <Link to={`./Contact`}>Contact</Link>
         </ul>
+        <button className="login"><img src={login} alt="loginIco" /></button>
       </div>
     </header>
   );
