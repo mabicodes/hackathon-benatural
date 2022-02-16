5716170cd31705c3362e7c04f57bc585c5a4dd99
Miguel-Lopez-97 <miguel.m.lopez.p@gmail.com>
2022-02-16 16:45:53 -0500

    initialization Carousel Component

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { MenuHome } from "./menu-home/MenuHome";
+import { Carousel } from "./carousel/Carousel";
 
 export const Home = () =>  {
     return (
         <div className="home"> 
+            <Carousel/>
             escribe codigo aqui Home
             <MenuHome/>
         </div>
---
+++ client/src/components/home/carousel/Carousel.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Carousel = () =>  {
+    return (
+        <div className="carousel"> 
+            escribe codigo aqui carousel
+        </div>
+    )
+}
