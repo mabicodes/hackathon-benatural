1740a1a68d251849cb6e357cc6ddaf23bce17ee4
carolo180 <caponeris1@gmail.com>
2022-02-17 05:46:03 -0500

    upload carrusel changes

--- client/src/components/home/carousel/Carousel.jsx
+++ client/src/components/home/carousel/Carousel.jsx
@@ -1,10 +1,37 @@
 import React from "react";
 import "./carousel.css";
+import fondo from "../../../assets/img/fondo_.png"
 
 export const Carousel = () =>  {
     return (
-        <div className="carousel"> 
-            escribe codigo aqui carousel
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
         </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+          <div id="carrusel1">
+            <h1 id="parrafo" alt="..." >hola mundo</h1>
+          </div>
+          </div>
+          <div class="carousel-item">
+            <img src={fondo} class="d-block w-100" alt="..."/>
+          </div>
+          <div class="carousel-item">
+            <img src={fondo} class="d-block w-100" alt="..."/>
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+  
     )
 }
--- client/src/components/main.jsx
+++ client/src/components/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Navbar } from "./navbar/Navbar";
+import { Carousel } from "./home/carousel/Carousel";
 import { Footer } from "./footer/Footer";
 import { Outlet } from "react-router-dom";
 
