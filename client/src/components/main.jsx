5c57b19998a69db6760f0e1c44bc42219acc1dca
Miguel-Lopez-97 <miguel.m.lopez.p@gmail.com>
2022-02-17 07:12:15 -0500

    Fix css footer

--- client/src/components/main.jsx
+++ client/src/components/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Navbar } from "./navbar/Navbar";
-import { Carousel } from "./home/carousel/Carousel";
 import { Footer } from "./footer/Footer";
 import { Outlet } from "react-router-dom";
 
