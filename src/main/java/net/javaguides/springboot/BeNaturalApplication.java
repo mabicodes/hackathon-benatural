703bf84fdab25240bccf70b033a1ad327de8efe4
Miguel-Lopez-97 <miguel.m.lopez.p@gmail.com>
2022-02-17 15:45:24 -0500

    Fix naturalAplication and test incomplete

--- src/main/java/net/javaguides/springboot/BeNaturalApplication.java
+++ src/main/java/net/javaguides/springboot/BeNaturalApplication.java
@@ -2,15 +2,10 @@ package net.javaguides.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import net.javaguides.springboot.benatural.repository.ProductRepository;
 
 @SpringBootApplication
 public class BeNaturalApplication {
     public static void main(String[] args) {
-        ConfigurableApplicationContext configurableApplicationContext =
         SpringApplication.run(BeNaturalApplication.class, args);
-        ProductRepository productRepository =
-        configurableApplicationContext.getBean(ProductRepository.class);
-    }
+       }
 }
