48345b0183d761c9984f13066b8c3679094e2e8c
Miguel-Lopez-97 <miguel.m.lopez.p@gmail.com>
2022-02-17 02:56:44 -0500

    CSS popupmodal

--- client/src/components/navbar/modal/PopupModal.jsx
+++ client/src/components/navbar/modal/PopupModal.jsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import './popupmodal.css'
+import naturalyLogo from "../../../assets/img/leaft.png"
 
 
 
@@ -7,30 +8,35 @@ function PopupModal(props) {
     return (props.trigger) ? (
         <div className="popup">
             <div className="popup-inner">
+            <div className="banner">
+                <div className="nameCompany"><h1>Be Natural</h1></div>
+                <div className="sloganCompany"><h3>LOGIN</h3></div>
+                <div className="logoCompany"><img src={naturalyLogo} alt="NaturalyLogo" /></div>
+            </div>
                 <button className="close-btn" onClick={() => props.setTrigger(false)}> X </button>
                 {props.children}
                 <form>
-                <div className="form">
-                <div className="form-element">
-                    <div className="form-label">
-                        <label htmlFor="email">Email</label></div>
-                    <input type="text" id="email" name="email" placeholder="example@benatural.org" autofocus/>
-                </div>
-                <div className="form-element">
-                    <div className="form-label">
-                        <label htmlFor="password">Password</label></div>
-                    <input type="password" id="password" name="password" placeholder="* * * * *"/>
+                    <div className="form">
+                        <div className="form-element">
+                            <div className="form-label">
+                                <label htmlFor="email">Email</label></div>
+                            <input type="text" id="email" name="email" placeholder="example@benatural.org" autofocus />
+                        </div>
+                        <div className="form-element">
+                            <div className="form-label">
+                                <label htmlFor="password">Password</label></div>
+                            <input type="password" id="password" name="password" placeholder="* * * * *" />
 
-                </div>
-                    <button className="form-login" type="submit" value="login">
-                         Log in </button>
+                        </div>
+                        <button className="form-login" type="submit" value="login">
+                            Log in </button>
 
 
-                </div>
+                    </div>
                 </form>
             </div>
         </div>
-    ) : "" ;
+    ) : "";
 }
 
 export default PopupModal
