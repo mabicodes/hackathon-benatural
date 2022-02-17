6514a10ea16954bb01053e87f8a5f0c90b6c0133
Marvie Nicole Uy <marvienicolebtw@gmail.com>
2022-02-17 17:34:25 +0100

    working with axios

--- src/main/java/net/javaguides/springboot/benatural/controller/ProductController.java
+++ src/main/java/net/javaguides/springboot/benatural/controller/ProductController.java
@@ -5,42 +5,31 @@ import net.javaguides.springboot.benatural.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
-
 @RestController
-// @RequestMapping("/api/products") No se si es requerido
-
+@CrossOrigin
 public class ProductController {
-    
-    private ProductRepository productRepository;
-    
     @Autowired
-
+    private ProductRepository productRepository;
     public ProductController(ProductRepository productRepository){
         this.productRepository = productRepository;
     }
-
     @GetMapping("/product/all")
     public ResponseEntity<List<Product>> getProducts(){
         return ResponseEntity.ok(
             this.productRepository.findAll()
         );
     }
-
     @PostMapping("/product/save")
     public Product saveProduct(@RequestBody Product product){
         return this.productRepository.save(product);
     }
-
     @GetMapping("/product/{id}")
     public ResponseEntity<Product> getProduct(@PathVariable(value = "id") Long id){
         Product product = this.productRepository.findById(id).orElseThrow(null);
-
         return ResponseEntity.ok().body(product);
     }
-
     @PutMapping("product/{id}")
     public Product updateProduct(@RequestBody Product newProduct, @PathVariable(value = "id") Long id){
         return this.productRepository.findById(id)
@@ -50,21 +39,16 @@ public class ProductController {
                 product.setProductCategory (newProduct.getProductCategory());
                 product.setProductPrice (newProduct.getProductPrice());
                 return this.productRepository.save(product);
-
             })
             .orElseGet(()->{
                 newProduct.setId(id);
                 return this.productRepository.save(newProduct);
             });       
     }
-
     @DeleteMapping("product/{id}")
     public ResponseEntity<Void> removeProduct(@PathVariable(value = "id") Long id) {
         Product product = this.productRepository.findById(id).orElseThrow(null);
-
         this.productRepository.delete(product);
         return ResponseEntity.ok().build();
-
     }
 }
-
