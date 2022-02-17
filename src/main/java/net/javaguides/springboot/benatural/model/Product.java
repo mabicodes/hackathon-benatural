6fdf301d5333274a3b8572541c14638b2d4245a2
Marvie Nicole Uy <marvienicolebtw@gmail.com>
2022-02-17 21:03:14 +0100

    rollback to last functional project

--- src/main/java/net/javaguides/springboot/benatural/model/Product.java
+++ src/main/java/net/javaguides/springboot/benatural/model/Product.java
@@ -1,17 +1,8 @@
 package net.javaguides.springboot.benatural.model;
-<<<<<<< HEAD
-=======
-import java.util.Set;
->>>>>>> 5d52a2beb8828cedc5396f854b7da9f87dee5b86
-
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.Data;
 
@@ -26,20 +17,15 @@ public class Product {
     private String productDescription;
     private String productCategory;
     private String productPrice;
-<<<<<<< HEAD
-
-    public Product(String productName, String productDescription, String productCategory, String productPrice){
-=======
     private String productImg;
     public Product(String productName, String productDescription, String productCategory, String productPrice, String productImg){
->>>>>>> 5d52a2beb8828cedc5396f854b7da9f87dee5b86
         this.productName = productName;
         this.productDescription = productDescription;
         this.productCategory = productCategory;
         this.productPrice = productPrice;
         this.productImg = productImg;
     }
-   
+    public Product(){}
     public Long getId() {return id;}
     public void setId(Long id) {this.id = id;}
 
@@ -67,27 +53,10 @@ public class Product {
     public void setProductPrice(String productPrice){
         this.productPrice = productPrice;
     }
-
     public String getProductImg() {
         return productImg;
     }
     public void setProductImg(String productImg) {
         this.productImg = productImg;
     }
-
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "id=" + id +
-                ", productName='" + productName + '\'' +
-                ", productDescription='" + productDescription + '\'' +
-                ", productCategory='" + productCategory + '\'' +
-                ", productPrice=" + productPrice  + '\'' +
-                '}';
-    }
-
-
-   
-
 }
